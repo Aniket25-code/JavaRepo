4a6ec820411743118dd193c4bb0a4439d6f4dd27
Gurav <aniket-tatoba.gurav@capgemini.com>
2025-04-15 15:04:15 +0530

    1 modification

--- Java_Practise/demo/src/main/Sum.java
+++ Java_Practise/demo/src/main/Sum.java
@@ -2,7 +2,7 @@ package Java_Practise.demo.src.main;
 
 public class Sum {
     public static void main(Strings[] args){
-        int num1= 50;
+        int num1= 200;
         int num2=100;
         int sum= num1+num2;
 
