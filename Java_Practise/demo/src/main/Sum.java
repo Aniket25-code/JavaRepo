808064e5b9e580d3bc835a320f94ec0f7d8315ff
Gurav <aniket-tatoba.gurav@capgemini.com>
2025-04-15 14:23:00 +0530

    My sum code

---
+++ Java_Practise/demo/src/main/Sum.java
@@ -0,0 +1,13 @@
+package Java_Practise.demo.src.main;
+
+public class Sum {
+    public static void main(Strings[] args){
+        int num1= 50;
+        int num2=100;
+        int sum= num1+num2;
+
+        System.out.println("sumation of two number is" +sum);
+    }
+
+    
+}
